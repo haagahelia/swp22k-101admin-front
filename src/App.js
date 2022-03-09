9d6ce6dd2e1e05c75a2ecd96072953824e773486
miikke79 <74132243+miikke79@users.noreply.github.com>
2022-03-09 11:12:39 +0200

    React App Creation

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+          Wolt Admin App
+      
+    </div>
+  );
+}
+
+export default App;
